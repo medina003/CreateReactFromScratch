3120c18a805ba6e9a05764e800d47b7bd3d72f54
Абасова Медина Искендер <Abas_ao63@itstep.edu.az>
2023-10-02 12:14:54 +0400

    develop

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Paragraph from './Components/Paragraph';
+
+function App(){
+    return (
+    <Paragraph text={'hellO'}></Paragraph>
+    )
+}
+export default App;
